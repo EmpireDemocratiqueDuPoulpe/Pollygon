aace012782e6d8d11871277991a2f45bddb190ff
alexi <293287@supinfo.com>
2020-05-13 19:02:28 +0200

    Mise à jour du css de la nav bar

--- views/models/navbar.php
+++ views/models/navbar.php
@@ -1,7 +1,8 @@
 <div id="navbar">
-    <a href="./home.php"><img src="./assets/images/pollygon_white.png" alt="Logo" title="Accueil"></a>
-    <div id="navbar_i">
-        <a href="./my_account.php"><i class="fas fa-user-circle fa-2x" title="Mon compte"></i></a>
-        <i class="fas fa-power-off fa-2x" title="Se déconnecter"></i>
+    <a id="navbarLogo" href="./home.php"><img src="./assets/images/pollygon_white.png" alt="Logo"></a>
+
+    <div id="navbar_links">
+        <a class="navbarIcon" href="./my_account.php"><i class="fas fa-user-circle fa-2x"></i></a>
+        <a class="navbarIcon" href="./php/user/disconnect.php"><i class="fas fa-power-off fa-2x"></i></a>
     </div>
 </div>
