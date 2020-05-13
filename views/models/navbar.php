a81d0e4908bf670246997798c038a048c71b479b
Breadator258 <maxence.pawlowski@supinfo.com>
2020-05-13 18:48:51 +0200

    Merge branch 'master' of https://github.com/EmpireDemocratiqueDuPoulpe/2PROJ-Pollygon
     Conflicts:
    	assets/css/main.css

--- views/models/navbar.php
+++ views/models/navbar.php
@@ -1,3 +1,7 @@
 <div id="navbar">
-
+    <a href="./home.php"><img src="./assets/images/pollygon_white.png" alt="Logo" title="Accueil"></a>
+    <div id="navbar_i">
+        <a href="./my_account.php"><i class="fas fa-user-circle fa-2x" title="Mon compte"></i></a>
+        <i class="fas fa-power-off fa-2x" title="Se déconnecter"></i>
+    </div>
 </div>
