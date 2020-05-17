569b1be6a39a10244aa9ae5423b0097585e67f4a
Breadator258 <maxence.pawlowski@supinfo.com>
2020-05-17 19:05:42 +0200

    SUPPRESSION DES SURVEYS CHOISIS

--- php/survey/delete.php
+++ php/survey/delete.php
@@ -1,2 +1,22 @@
 <?php
-var_dump($_POST);
+
+require_once "../../init.php";
+
+$surveys_to_delete = $_POST["survey_delete"];
+
+
+foreach ($surveys_to_delete as $survey){
+    $surveys = PDOFactory::sendQuery(
+        $db,
+        'DELETE FROM surveys WHERE owner_id = :owner_id AND survey_id = :survey_id',
+        ["owner_id" => $_SESSION["user_id"],
+         "survey_id" => intval($survey)], false
+    );
+    ob_start();
+}
+
+header('Location: ../../home.php');
+
+
+
+
