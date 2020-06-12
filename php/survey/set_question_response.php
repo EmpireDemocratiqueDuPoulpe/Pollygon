d3258e7bf307a5b30e0f5c3847cd18531376cfea
alexi <293287@supinfo.com>
2020-06-12 15:31:03 +0200

    Réglage de deux bugs permettant de ne pas répondre à la dernière question et d'augmenter le nombre de participants

--- php/survey/set_question_response.php
+++ php/survey/set_question_response.php
@@ -1,6 +1,7 @@
 <?php
 require_once "../../init.php";
 
+$SurveyManager = new Survey($db);
 $AnswerManager = new Answer($db);
 
 ############################
@@ -29,39 +30,68 @@ if (is_null($answer_id)) { setError(ANSWER_NOT_FOUND); redirectTo(ANSWER_SURVEY_
 // INPUT
 if (!is_null($answer_response_input)) {
 
-    ############################
-    # Check the question response
-    ############################
-
     $answer_response = trim($answer_response_input);
 
-    if (strlen($answer_response) == 0) {
+    if (empty($answer_response)) {
         setError(ANSWER_NOT_VALID);
         redirectTo(ANSWER_SURVEY_PAGE.$URI);
     }
 
 // UNIQUE
 } else if (!is_null($answer_response_unique)) {
+
     $answer_response = trim($answer_response_unique);
+
+    if (empty($answer_response)) {
+        setError(ANSWER_NOT_VALID);
+        redirectTo(ANSWER_SURVEY_PAGE.$URI);
+    }
+
 // MULTIPLE
 } else if (!is_null($answer_response_multiple)) {
+
     $answer_response = str_replace("%", "&#37;", $answer_response_multiple);
     $answer_response = trim(implode("%", $answer_response_multiple));
+
+    if (empty($answer_response)) {
+        setError(ANSWER_NOT_VALID);
+        redirectTo(ANSWER_SURVEY_PAGE.$URI);
+    }
+
 // NUMBER
 } else if (!is_null($answer_response_number)) {
+
     $answer_response = filter_var(trim($answer_response_number), FILTER_VALIDATE_FLOAT);
 
     if (!is_int($answer_response) AND !is_float($answer_response) AND !is_double($answer_response)) {
         setError(ANSWER_NOT_VALID);
         redirectTo(ANSWER_SURVEY_PAGE.$URI);
     }
+} else {
+    setError(ANSWER_NOT_VALID);
+    redirectTo(ANSWER_SURVEY_PAGE.$URI);
 }
 
 ############################
 # Set the response
 ############################
 
-if (!$AnswerManager->setValue($answer_id, $_SESSION["user_id"], $answer_response))
+if (!$AnswerManager->setValue($answer_id, $_SESSION["user_id"], $answer_response)) {
     setError(UNKNOWN_ANSWER_ADD_ERROR);
+    redirectTo(ANSWER_SURVEY_PAGE.$URI);
+}
 
-redirectTo(ANSWER_SURVEY_PAGE.$URI);
+
+############################
+# Check if the survey is answered
+############################
+
+if (!$AnswerManager->isLastAnswer($survey_id, $_SESSION["user_id"])) {
+    redirectTo(ANSWER_SURVEY_PAGE.$URI);
+} else {
+    $SurveyManager->addMembers($survey_id, 1);
+
+    setSuccess(SURVEY_ANSWERED);
+    redirectTo(HOME_PAGE);
+}
+
